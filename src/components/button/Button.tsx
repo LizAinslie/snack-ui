eacb58cecb0f909a462c27bc5561c708274a994d
Liz Ainslie <lizainslie16@gmail.com>
2023-03-03 16:40:48 -0700

    chore(lib): properly type button, add prettier hook

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,41 +1,50 @@
-import { 
+import {
   ComponentPropsWithoutRef,
   ElementType,
+  FC,
   forwardRef,
-  PropsWithChildren
+  PropsWithChildren,
 } from "react";
 import { clsx } from "clsx";
 
 import { PolymorphicRef } from "../../util/types/polymorphic";
 import { PropsWithColor } from "../../util/types/propTypes";
 
-import styles from './Button.module.scss';
+import styles from "./Button.module.scss";
 
 type ButtonProps<C extends ElementType> = {
   as?: C;
-  block?: boolean,
-  muted?: boolean,
-} & PropsWithChildren & PropsWithColor & ComponentPropsWithoutRef<C>;
+  block?: boolean;
+  muted?: boolean;
+} & PropsWithChildren &
+  PropsWithColor &
+  ComponentPropsWithoutRef<C>;
 
-export const Button = forwardRef(<C extends ElementType = "button">({
-  block = false,
-  muted = false,
-  color,
-  children,
-  as,
-  ...rest
-}: ButtonProps<C>, ref: PolymorphicRef<C>) => {
-  const Component = as || 'button';
-  
-  return (
-    <Component
-      ref={ref} 
-      className={clsx(styles.button, styles[`${color}`], {
-        [styles.block]: block,
-        [styles.muted]: muted,
-      })}
-      {...rest}
-    >{children}</Component>
-  );
-});
+export const Button: FC<ButtonProps<ElementType<any>>> = forwardRef(
+  <C extends ElementType = "button">(
+    {
+      block = false,
+      muted = false,
+      color,
+      children,
+      as,
+      ...rest
+    }: ButtonProps<C>,
+    ref: PolymorphicRef<C>
+  ) => {
+    const Component = as || "button";
 
+    return (
+      <Component
+        ref={ref}
+        className={clsx(styles.button, styles[`${color}`], {
+          [styles.block]: block,
+          [styles.muted]: muted,
+        })}
+        {...rest}
+      >
+        {children}
+      </Component>
+    );
+  }
+);
--- src/demo/DemoApp.tsx
+++ src/demo/DemoApp.tsx
@@ -3,16 +3,18 @@ import { Button } from "../components/button";
 import PageContainer from "../components/pageContainer/PageContainer";
 
 const DemoApp: FC = () => {
-  const [color, setColor] = useState('gray');
+  const [color, setColor] = useState("rose");
   const [buttonClicks, setButtonClicks] = useState(0);
 
   return (
     <PageContainer color={color} fullPage>
-      <div className='p-4'>
-        <Button 
+      <div className="p-4">
+        <Button
           color={color}
-          onClick={() => void setButtonClicks(prev => prev + 1)}
-        >I've been clicked {buttonClicks} times</Button>
+          onClick={() => void setButtonClicks((prev) => prev + 1)}
+        >
+          I've been clicked {buttonClicks} times
+        </Button>
       </div>
     </PageContainer>
   );
