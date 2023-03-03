f55706c4ee8d3383761cee23f32881677c737582
Liz Ainslie <lizainslie16@gmail.com>
2023-03-03 16:43:59 -0700

    chore(lib): properly type page container

--- src/components/pageContainer/PageContainer.tsx
+++ src/components/pageContainer/PageContainer.tsx
@@ -1,41 +1,50 @@
-import clsx from 'clsx';
+import clsx from "clsx";
 import {
-  ComponentPropsWithoutRef, 
-  ElementType, 
-  forwardRef, 
-  PropsWithChildren
-} from 'react';
-import { PolymorphicRef } from '../../util/types/polymorphic';
-import { PropsWithColor, PropsWithDark } from '../../util/types/propTypes';
-import styles from './PageContainer.module.scss';
+  ComponentPropsWithoutRef,
+  ElementType,
+  FC,
+  forwardRef,
+  PropsWithChildren,
+} from "react";
+import { PolymorphicRef } from "../../util/types/polymorphic";
+import { PropsWithColor, PropsWithDark } from "../../util/types/propTypes";
+import styles from "./PageContainer.module.scss";
 
 export type PageContainerProps<C extends ElementType> = {
   as?: C;
   fullPage: boolean;
-} & PropsWithColor & PropsWithDark & PropsWithChildren 
-  & ComponentPropsWithoutRef<C>;
+} & PropsWithColor &
+  PropsWithDark &
+  PropsWithChildren &
+  ComponentPropsWithoutRef<C>;
 
-const PageContainer = forwardRef(<C extends ElementType = "div",>({
-  fullPage = false,
-  dark = false,
-  color,
-  as,
-  children,
-  ...rest
-}: PageContainerProps<C>, ref: PolymorphicRef<C>) => {
-  const Component = as || 'div';
+const PageContainer: FC<PageContainerProps<ElementType<any>>> = forwardRef(
+  <C extends ElementType = "div">(
+    {
+      fullPage = false,
+      dark = false,
+      color,
+      as,
+      children,
+      ...rest
+    }: PageContainerProps<C>,
+    ref: PolymorphicRef<C>
+  ) => {
+    const Component = as || "div";
 
-  return (
-    <Component
-      ref={ref}
-      className={clsx(styles.pageContainer, styles[`${color}`], {
-        [styles.fullPage]: fullPage,
-        [styles.dark]: dark,
-      })}
-      {...rest}
-    >{children}</Component>
-  );
-});
+    return (
+      <Component
+        ref={ref}
+        className={clsx(styles.pageContainer, styles[`${color}`], {
+          [styles.fullPage]: fullPage,
+          [styles.dark]: dark,
+        })}
+        {...rest}
+      >
+        {children}
+      </Component>
+    );
+  }
+);
 
 export default PageContainer;
-
